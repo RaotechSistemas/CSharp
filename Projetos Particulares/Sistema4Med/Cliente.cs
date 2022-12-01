605fd442a87daa25948de48976de9098e293a00c
Reginaldo Araujo Oliveira <rao.corretor@gmail.com>
2022-12-01 07:57:16 -0300

    Projeto 4med em CSharp
    1º Dia

---
+++ Projetos Particulares/Sistema4Med/Cliente.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Cliente
+    {
+        public int id_Cliente {get; set;}
+        public string nome_Cliente {get;set;}
+        public string cpf_Cliente { get; set; }
+        public string rg_Cliente { get; set; }
+        
+    }
+}
